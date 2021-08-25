6a9cb60c0fd2ae43db9aae495626d19f1c12f7ab
Kevin <kevinsevinche@gmail.com>
2021-08-25 13:30:05 -0500

    Update components to use middlewares

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
+import { removeBooksThunk, getBooksThunk } from '../redux/books/books';
 import Book from './Book';
 
 const Books = () => {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooksThunk());
+  }, []);
+
   const books = useSelector((state) => state.booksReducer);
 
   const handleClick = (event) => {
-    dispatch(removeBook(event.target.value));
+    dispatch(removeBooksThunk(event.target.value));
   };
 
   return (
     <main>
       <ul className="">
         {books.map((book) => (
-          <Book key={book.id} book={book} handleClick={handleClick} />
+          <Book key={book[0]} book={book} handleClick={handleClick} />
         ))}
       </ul>
     </main>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,59 +1,63 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/books';
+import { addBookThunk } from '../redux/books/books';
 
 const Form = () => {
   const dispatch = useDispatch();
 
   const [newTitle, setNewTitle] = useState('');
-  const [newAuthor, setNewAuthor] = useState('');
+  const [newCategory, setNewCategory] = useState('');
 
   const getTitleInput = (event) => {
     setNewTitle(event.target.value);
   };
 
-  const getAuthorInput = (event) => {
-    setNewAuthor(event.target.value);
+  const getCategoryInput = (event) => {
+    setNewCategory(event.target.value);
   };
 
   const submitBookToStore = (event) => {
     event.preventDefault();
     const newBook = {
-      id: uuidv4(),
-      name: newTitle,
-      author: newAuthor,
+      item_id: uuidv4(),
+      title: newTitle,
+      category: newCategory,
     };
-    dispatch(addBook(newBook));
+    dispatch(addBookThunk(newBook));
+    setNewTitle('');
+    setNewCategory('');
   };
 
   return (
     <section className="addForm">
       <h2>ADD NEW BOOK</h2>
-      <form action="">
+      <form action="" onSubmit={submitBookToStore}>
         <input
           type="text"
           placeholder="Book title"
           required
           onChange={getTitleInput}
+          value={newTitle}
         />
-        <input
-          type="text"
-          placeholder="Author"
+        <select
+          name="category"
+          placeholder="cat"
+          onChange={getCategoryInput}
           required
-          onChange={getAuthorInput}
-        />
-        <select name="category" placeholder="cat">
+        >
           <option value="" disabled selected>
             Category
           </option>
-          <option value="action">Action</option>
-          <option value="science Fiction">Science Fiction</option>
-          <option value="economy">Economy</option>
+          <option value="Action">Action</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Economy">Economy</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="Mystery">Mystery</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romance">Romance</option>
         </select>
-        <button type="submit" onClick={submitBookToStore}>
-          ADD BOOK
-        </button>
+        <button type="submit">ADD BOOK</button>
       </form>
     </section>
   );
