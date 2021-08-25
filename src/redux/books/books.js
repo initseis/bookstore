6700733263888ec4dc33924935f4e052d3a4db47
Kevin <kevinsevinche@gmail.com>
2021-08-25 13:31:37 -0500

    Add thunk middlewares

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,29 +1,63 @@
+import { getBooksAPI, setBookAPI, deleteBookAPI } from '../../controllers/api';
+
+const GET_BOOKS = 'bookStore/books/GET_BOOKS';
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-export const addBook = (payload) => ({
+const getBooks = (payload) => ({
+  type: GET_BOOKS,
+  payload,
+});
+
+const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
 });
 
-export const removeBook = (payload) => ({
+const removeBook = (payload) => ({
   type: REMOVE_BOOK,
   payload,
 });
 
-const initialState = [];
+const initialState = [['', ['']]];
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_BOOKS:
+      return action.payload;
+
     case ADD_BOOK:
       return [...state, action.payload];
 
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => String(book[0]) !== String(action.payload));
 
     default:
       return state;
   }
+};
+
+export const getBooksThunk = () => function (dispatch) {
+  getBooksAPI().then((res) => {
+    dispatch(getBooks(Object.entries(res)));
+  });
+};
+
+export const addBookThunk = (newBook) => function (dispatch) {
+  setBookAPI(newBook).then(() => {
+    dispatch(
+      addBook([
+        newBook.item_id,
+        [{ category: newBook.category, title: newBook.title }],
+      ]),
+    );
+  });
+};
+
+export const removeBooksThunk = (id) => function (dispatch) {
+  deleteBookAPI(id).then(() => {
+    dispatch(removeBook(id));
+  });
 };
 
 export default reducer;
